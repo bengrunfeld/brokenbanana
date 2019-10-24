afa8f3313b0febcf6082ae5531c9ddd39118d956
bengrunfeld <benjamin.grunfeld@gmail.com>
2019-10-24 22:07:00 +1100

    Remove console statement

--- components/Teaser/Teaser.tsx
+++ components/Teaser/Teaser.tsx
@@ -1,14 +1,11 @@
 import { TeaserContent, TeaserProps } from "./types";
 
-const Teaser = ({ content }: TeaserProps) => {
-  console.log("->", content.img);
-  return (
-    <article>
-      <h2>{content.title}</h2>
-      <p>{content.excerpt}</p>
-      <img src={content.img} />
-    </article>
-  );
-};
+const Teaser = ({ content }: TeaserProps) => (
+  <article>
+    <h2>{content.title}</h2>
+    <p>{content.excerpt}</p>
+    <img src={content.img} />
+  </article>
+);
 
 export default Teaser;
