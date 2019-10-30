ae7df3e621140d7f93dbc7d2e0ab47ee111369bf
bengrunfeld <benjamin.grunfeld@gmail.com>
2019-10-30 11:38:01 +1100

    Change api request to https

--- utils/getUrlPrefix.ts
+++ utils/getUrlPrefix.ts
@@ -2,7 +2,7 @@ const getUrlPrefix = () => {
   console.log("-> DEV/PROD:", process.env.NODE_ENV);
 
   if (process.env.NODE_ENV === "development") return "http://localhost:3000";
-  if (process.env.NODE_ENV === "production") return "http://brokenbanana.tech";
+  if (process.env.NODE_ENV === "production") return "https://brokenbanana.tech";
   return "http://brokenbanana.tech";
 };
 
